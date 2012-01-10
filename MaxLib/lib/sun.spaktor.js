7d6f28e1c664f1c020c42719453379290d3727ac
howthebodyworks <git@email.possumpalace.org>
2012-01-10 19:12:49 +1100

    Fought my way past teh bloody-mindedly stupid LiveAPI business

--- MaxLib/lib/sun.spaktor.js
+++ MaxLib/lib/sun.spaktor.js
@@ -1,16 +1,84 @@
-var api;
+/* 
+Live loop-point wrangling for Live, a la certain Spative Spinstruments
+products.
+
+TODO:
+
+* (optionally?) reset previous loop point state upon the playing clip being
+  changed.
+* remember all loop points for all clips
+* ...and persist across sessions?
+
+*/
+var track;
+var track_id;
+var track_path;
+var current_clip;
+var current_clip_slot_path;
+var current_clip_slot;
+var clip_meta = {};
 
 function bang() {
+  var playing_idx;
+  var playing_id;
   //init
-  api = new LiveAPI();
-  api.path = "this_device";
-  post("I am", api.id, api.info);
+  track = new LiveAPI(null, "this_device canonical_parent");
+  track_id = track.id;
+  // look at this stupidity: LiveAPI.path returns a string with *embedded
+  // quotes*. I can't believe the world can continue under such a burden of
+  // dumb.
+  track_path = track.path.slice(1,-1);
+  post("track", track_path, track_id);
   post();
-  post("My parent", api.get("canonical_parent"));
+  
+  // playing_slot_index, fire_slot_index
+  playing_idx = track.get("playing_slot_index");
+  
+  // check out this indexing bullshit - every second entry in the array is the
+  // string "id". FFS.
+  playing_id = track.get('clip_slots')[playing_idx*2+1];
+  post("clipsy", "IDX", playing_idx, "ID", playing_id, "type", typeof(playing_id));
   post();
-  api.goto("this_device canonical_parent");
-  post("My parent2", api.id, api.info);
+
+  current_clip_slot_path = track.path.slice(1,-1) + " clip_slots "+ track.id;
+  current_clip_slot = new LiveAPI(null, current_clip_slot_path);
+  current_clip = new LiveAPI(null, current_clip_slot_path+ " clip");
+  //current_clip.id = track_id;
+  post("trackb", current_clip.id, current_clip.type);
+  //current_clip.goto("clip_slots " + playing_id);
+  post("clip", current_clip.info);
+  post("clip_slots " + playing_idx, current_clip.info);
+  post();
+  post("track", track.id);
   post();
 }
 
-// function 
+//Clip members
+/*
+js: property is_audio_clip bool
+js: property is_midi_clip bool
+js: property is_playing bool
+js: property is_recording bool
+js: property is_triggered bool
+js: property length float
+js: property loop_end float
+js: property loop_start float
+js: property looping bool
+js: property muted bool
+js: property name unicode
+js: property pitch_coarse int
+js: property pitch_fine float
+js: property playing_position float
+js: property signature_denominator int
+js: property signature_numerator int
+js: property warping bool
+js: property will_record_on_start bool
+js: function deselect_all_notes
+js: function fire
+js: function get_selected_notes
+js: function move_playing_pos
+js: function replace_selected_notes
+js: function select_all_notes
+js: function set_fire_button_state
+js: function stop
+*/
