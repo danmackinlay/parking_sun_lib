fb88207e572c94d8f52b57446b87d1ffec18db3b
howthebodyworks <git@email.possumpalace.org>
2012-01-10 16:45:04 +1100

    go to the parent of the current thingy

--- MaxLib/lib/sun.spaktor.js
+++ MaxLib/lib/sun.spaktor.js
@@ -6,6 +6,11 @@ function bang() {
   api.path = "this_device";
   post("I am", api.id, api.info);
   post();
+  post("My parent", api.get("canonical_parent"));
+  post();
+  api.goto("this_device canonical_parent");
+  post("My parent2", api.id, api.info);
+  post();
 }
 
 // function 
